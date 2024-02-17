3a2c2c86273d2a45f74e90daf2a6e6d49e89976d
alokiksingh1 <alokiksingh.kinra@ucalgary.ca>
2024-02-17 15:27:56 -0700

    sites.js and schema fixed; csv teting done

--- web/server/models/Site.js
+++ web/server/models/Site.js
@@ -1,36 +1,26 @@
 // Initiate a connection to MongoDB Atlas and insert sample data
 
-const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Connect to MongoDB Atlas (replace connection string with your actual MongoDB Atlas connection string)
-mongoose.connect('mongodb+srv://alokiksingh2:historicsitefinder@historicsiteffnder.eul1kwi.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("Connected to MongoDB Atlas");
-  // Define User schema
-  const siteSchema = new mongoose.Schema({
+
+const siteSchema = new mongoose.Schema({
     sitename: { type: String, required: true },
     sitelocation: { type: String, required: true },
-    site_community: { type: String, required: true },
-    site_constructiondate: { type: String, required: true },
-    site_typology: { type: String, required: true },
-    site_OriginalOwner: { type: String, required: true },
-    site_masterPlan: { type: String, required: true },
-    site_Architect: { type: String, required: true },
-    site_ArchitecturalStyle: { type: String, required: true },
-    site_era: { type: String, required: true },
-    site_currentUse: { type: String, required: true },
-    site_Demolished: { type: String, required: true },
-    sitedescription: { type: String, required: true },
-    site_URL: { type: String, required: true },
-  //  site_rating: { type: String, required: true },
-  });
+    site_community: { type: String, required: false },
+    site_constructiondate: { type: String, required: false },
+    site_typology: { type: String, required: false },
+    site_OriginalOwner: { type: String, required: false },
+    site_masterPlan: { type: String, required: false },
+    site_Architect: { type: String, required: false },
+    site_ArchitecturalStyle: { type: String, required: false },
+    site_era: { type: String, required: false },
+    site_currentUse: { type: String, required: false },
+    site_Demolished: { type: String, required: false },
+    sitedescription: { type: String, required: false },
+    site_URL: { type: String, required: false },
+    //  site_rating: { type: String, required: true },
+});
 
   // Create the model using the schema
 const SiteModel = mongoose.model('Site', siteSchema);
@@ -38,14 +28,13 @@ const SiteModel = mongoose.model('Site', siteSchema);
 // Export the model
 module.exports = SiteModel;
 
-})
-.catch((error) => {
-  console.error("Error connecting to MongoDB Atlas:", error);
-});
+// .catch((error) => {
+//   console.error("Error connecting to MongoDB Atlas:", error);
+// });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 
