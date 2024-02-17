c54a606b68a482572bf872539ba97f859b6af93e
tentdorj <144164843+tentdorj@users.noreply.github.com>
2024-02-17 14:02:03 -0700

    created user schema for db

--- web/server/models/User.js
+++ web/server/models/User.js
@@ -1,36 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// // Connect to MongoDB
-// mongoose.connect('mongodb+srv://alokiksingh2:<historicsitefinder>@historicsiteffnder.eul1kwi.mongodb.net/?retryWrites=true&w=majority', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-
-
-// const sampleUserData = {
-//     userToken: 'abc123',
-//     username: 'john_doe'
-//   };
-  
-//   // Insert sample data into MongoDB Atlas
-//   async function insertSampleData() {
-//     try {
-//       await UserModel.create(sampleUserData);
-//       console.log('Sample data inserted successfully.');
-//     } catch (error) {
-//       console.error('Error inserting sample data:', error);
-//     }
-//   }
-  
-//   // Call the function to insert sample data
-//   insertSampleData();
-
-
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -46,30 +13,18 @@ mongoose.connect('mongodb+srv://alokiksingh2:historicsitefinder@historicsiteffnd
   console.log("Connected to MongoDB Atlas");
   // Define User schema
   const userSchema = new mongoose.Schema({
-    userToken: { type: String, required: true },
-    username: { type: String, required: true },
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true, unique: true },
+    badge: { type: Number, required: true, unique: true },
+    comments: [{
+      description: { type: String, required: true },
+      date: { type: Date, required: true },
+      likes: { type: Number, required: true },
+    }]
   });
   // Create UserModel
   const UserModel = mongoose.model('User', userSchema);
-  
-  // Sample user data
-  const sampleUserData = {
-    userToken: 'abc123',
-    username: 'john_doe'
-  };
-  
-  // Insert sample data into MongoDB Atlas
-  async function insertSampleData() {
-    try {
-      await UserModel.create(sampleUserData);
-      console.log('Sample data inserted successfully.');
-    } catch (error) {
-      console.error('Error inserting sample data:', error);
-    }
-  }
-  
-  // Call the function to insert sample data
-  insertSampleData();
 })
 .catch((error) => {
   console.error("Error connecting to MongoDB Atlas:", error);
