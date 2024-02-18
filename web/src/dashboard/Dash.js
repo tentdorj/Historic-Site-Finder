1dc3aba21a04a8f5277d94e910ce81338dff589b
ArmaanMohar <armaan.mohar@gmail.com>
2024-02-17 21:42:02 -0700

    started maps cards

--- web/src/dashboard/Dash.js
+++ web/src/dashboard/Dash.js
@@ -2,13 +2,16 @@ import MyMap from "./MyMap";
 
 export default function Dash() {
   return (
-    <div className="d-flex min-vh-100 ">
+    <div className="d-flex min-vh-100 min-vw-100">
       <div className="row text-center w-100">
         <div className="col-4  bg-primary">
-          <p>site 1</p>
+          <div className="d-flex border rounded-pill">
+            <p>site 1</p>
+            <p>Name</p>
+          </div>
           <p>site 2</p>
         </div>
-        <div className="col bg-secndary">
+        <div className="col p-0  mx-auto bg-primary">
           <MyMap />
         </div>
       </div>
