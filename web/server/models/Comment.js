5bf19a56b8ee6495ebbbea8b55bc95f38a1c6d62
alokiksingh1 <alokiksingh.kinra@ucalgary.ca>
2024-02-18 01:15:13 -0700

    site_id

--- web/server/models/Comment.js
+++ web/server/models/Comment.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema({
   content: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  site_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Site', required: true },
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   createdAt: { type: Date, default: Date.now },
