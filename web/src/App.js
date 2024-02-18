977afbaa69108bc98feccd5f696fc372610984b2
GurleenA <101472021+GurleenA@users.noreply.github.com>
2024-02-17 17:19:41 -0700

    Adding Background images and formatting the login page

--- web/src/App.js
+++ web/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <NavBar />
       </header>
       {/* <Landing /> */}
- 
+    <Login/>
     </div>
   );
 }
--- web/src/loginSignUp/Login.js
+++ web/src/loginSignUp/Login.js
@@ -6,27 +6,35 @@ export default function Landing() {
      
     <div className="min-vh-100 d-flex align-items-center justify-content-center position-relative border-4 bg-img border">
         <img src={yycImage} alt="skyline" className=" img-fluid position-absolute object-fit-fill w-100 img-blur"/>
-        <div className="d-flex justify-content-center align-items-center flex-row w-75 position-absolute"> 
-        
-            {/* <div className="border w-30 h-50"> 
-                <img src={yycImage} alt="skyline" className="object-fit-contain"/>
-            </div> */}
-            {/* <div className="has-bg-img border w-30 h-30">
-                <img src={yycImage} alt="skyline" className="object-fit-contain"/>
-            </div> */}
-            <span className="border p-4 w-30 h-30 d-flex justify-content-center flex-column bg-info-subtle">
-                <h2 className="text-center">Login</h2>
-                <div className="form-group d-flex flex-column">
-                    <label className="mt-2">Username:</label>
-                    <input type="text" className="mb-3"></input>
-                    <label>Password:</label>
-                    <input type="password"></input>
-                </div>               
-                <div className="text-center">
-                    <button type="submit" class="btn btn-primary bg-black mt-4 mw-2 mb-2">Submit</button>
-                    <p>Dont have an account? <strong><u><a href="">Sign Up</a></u></strong></p>
+        {/* <div className="d-flex justify-content-center align-items-center flex-row w-75 position-absolute">  */}
+        <div className="d-flex flex-row position-absolute">
+            <div className="row">
+                <div className="bg-img border col p-0 position-relative">
+                    <img src={yycImage} alt="skyline" className="img-fluid w-100"/> 
                 </div>
-            </span>
+                <div className="col border d-flex justify-content-center align-items-center bg-white">
+                    <div className="p-3">
+                        <h2 className="">Login</h2>
+                        <div className="form-group d-flex flex-column">
+                            <label className="mt-2 login-text ">Username:</label>
+                            <div class="col-xs-3">  
+                                <input type="text"></input>
+                                <p className="forgot-text mb-3 ">Forgot Username? <strong><u><a href="" className="font-color">Click Here</a></u></strong></p>
+                            </div>
+                            <label  className="login-text">Password:</label>
+                            <div class="col-xs-3">  
+                                <input type="password"></input>
+                                <p className="forgot-text">Forgot Password? <strong><u><a href="" className="font-color">Click Here</a></u></strong></p>
+                            </div>
+                        </div> 
+                        {/* <div className="text-center"> */}
+                            <button type="submit" class="btn btn-submit mw-2 mb-2 mt-3">Submit</button>
+                            <p  className="">Dont have an account? <strong><u><a href="" className="font-color">Sign Up</a></u></strong></p>
+                        {/* </div> */}
+                    </div>              
+                    
+                </div>
+            </div>
         </div>
     </div>
     );
